5b41a42d42b458b81b9bef3b855a6c6b392fa120
Lizzie <lchan217@gmail.com>
2019-10-23 10:38:39 -0700

    reversestring complete

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,9 +6,28 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {
-  return str.split('').reverse().join('')
-  // turn array into string, reverse it, then join string again
-}
+// SOLUTION 1
+// function reverse(str) {
+//   return str.split('').reverse().join('')
+//   // turn array into string, reverse it, then join string again
+// }
+
+// // SOLUTION 2**
+// // don't use traditional for loop because of typo opportunities
+// function reverse(str) {
+//   let reversed = ''
+//   for (let character of str) {
+//     reversed = character + reversed
+//   }
+//   return reversed
+// }
+
+// // SOLUTION 3 ~2 min
+// function reverse(str){
+//   let array = str.split('')
+//   return array.reduce((reversed, character) => {
+//     return character + reversed
+//   }, '')
+// }
 
 module.exports = reverse;
