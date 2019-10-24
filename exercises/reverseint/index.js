79b1f3d95053fa5c3c60d17efb8e58c48786785e
Lizzie <lchan217@gmail.com>
2019-10-24 16:49:59 -0700

    done.

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,16 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+// My Solution (similar to suggested solution)
+// function reverseInt(n){
+//   let s = n.toString()
+//   let result = parseInt(s.split('').reverse().join(''))
+//   return n < 0 ? result*-1 : result
+// }
+
+//1st solution
+function reverseInt(n){
+  return Math.sign(n)*parseInt(n.toString().split('').reverse().join(''))
+}
 
 module.exports = reverseInt;
