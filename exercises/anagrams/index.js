591ae133513f523e7f7d96abea0e4e18599c254c
Lizzie <lchan217@gmail.com>
2019-10-27 12:41:58 -0700

    done

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,48 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// // MY SOLUTION (SOLUTION 2)
+// function anagrams(stringA, stringB) {
+//   let removedA = stringA.replace(/[^\w]/g, "")
+//   let removedB = stringB.replace(/[^\w]/g, "")
+//
+//   let strA = removedA.toLowerCase().split('').sort().join('')
+//   let strB = removedB.toLowerCase().split('').sort().join('')
+//
+//   return strA === strB ? true : false
+// }
 
+// SOLUTION 1
+function anagrams(stringA, stringB){
+    let arrayA = stringA.replace(/[^\w]/g, "").toLowerCase().split('')
+    let arrayB = stringB.replace(/[^\w]/g, "").toLowerCase().split('')
+
+    let objA = {}
+    let objB = {}
+
+    for (let element of arrayA){
+      if (objA[element]){
+        objA[element]++
+      } else
+      objA[element] = 1
+    }
+
+    for (let element of arrayB){
+      if (objB[element]){
+        objB[element]++
+      } else
+      objB[element] = 1
+    }
+
+    if (Object.keys(objA).length !== Object.keys(objB).length) {
+      return false
+    }
+
+    for (let chunk in objA){
+      if (objA[chunk] !== objB[chunk]){
+        return false
+      }
+    }
+    return true
+}
 module.exports = anagrams;
