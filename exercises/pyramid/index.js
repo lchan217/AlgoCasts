7742cbc1434b6ef95645afff69395e548b50d8c1
Lizzie <lchan217@gmail.com>
2019-10-28 15:07:09 -0700

    done.

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,25 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+//SOLUTION 1
+function pyramid(n) {
+  // take floor of the mid point... add 1 and subtract 1 for where to add #'s
+  const midpoint = Math.floor((2*n-1) / 2)
+
+  for (let row = 0; row < n; row++){
+    let level = ''
+
+  // the pattern - 2*n-1 = columns (see diagram)
+
+    for (let column = 0; column < 2*n-1; column++){
+      if (midpoint - row <= column && midpoint + row >= column) {
+        level += '#'
+      } else {
+        level +=' '
+      }
+    }
+    console.log(level)
+  }
+}
 
 module.exports = pyramid;
