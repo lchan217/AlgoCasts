b5b2f1b2550ab4651106a30d2b68aa33bd90b78e
Lizzie <lchan217@gmail.com>
2019-10-30 18:18:29 -0700

    done

--- exercises/weave/queue.js
+++ exercises/weave/queue.js
@@ -16,6 +16,27 @@ class Queue {
   remove() {
     return this.data.pop();
   }
+
+  peek(){
+    return this.data[this.data.length-1]
+  }
+
 }
+
+//GIVEN
+// class Queue {
+//   constructor() {
+//     this.data = [];
+//   }
+//
+//   add(record) {
+//     this.data.unshift(record);
+//   }
+//
+//   remove() {
+//     return this.data.pop();
+//   }
+//
+// }
 
 module.exports = Queue;
